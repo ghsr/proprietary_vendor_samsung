d4c839a4032d6d4f212959285c0d4dc12afbecd0
KINGbabasula <kingbabasula@gmail.com>
2013-10-21 15:14:29 +0200

    Renames

--- s2vep/s2vep-vendor.mk
+++ s2vep/s2vep-vendor.mk
@@ -27,6 +27,6 @@ PRODUCT_COPY_FILES := \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/samsung/i9105P/overlay
+DEVICE_PACKAGE_OVERLAYS := vendor/samsung/s2vep/overlay
 
-$(call inherit-product, vendor/samsung/i9105P/i9105P-vendor-blobs.mk)
+$(call inherit-product, vendor/samsung/s2vep/s2vep-vendor-blobs.mk)
